d0689fc8c5e5e60a0d6aae4191d15f3bd7e77fb2
AndrewSheu <andrewsheu0617@gmail.com>
2022-04-11 22:51:28 +1000

    one

---
+++ Assignment1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //testing add
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+
+            //testing delete
+
+
+
+            //testing search
+
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
